a1de19a35019ee75a30abe8f3a6423c3a6a19389
Alisha Priyadarshi <alishapriyadarshi098@gmail.com>
2025-08-19 06:57:30 +0000

    handler.js:191  GET https://chatverse-v8eax.firebaseapp.com/__/firebase/

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -9,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA2m3v7D8qjiK4C3SE3rLzH_NJbaE24304",
   authDomain: "chatverse-v8eax.firebaseapp.com",
   projectId: "chatverse-v8eax",
-  storageBucket: "chatverse-v8eax.firebasestorage.app",
+  storageBucket: "chatverse-v8eax.appspot.com",
   messagingSenderId: "895330788794",
   appId: "1:895330788794:web:0c2587553c246ab3ae2983"
 };
